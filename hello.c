5fbd3835203bde1366e677d62221425a62e3434d
ilyaigpetrov <ilyaigpetrov@gmail.com>
2018-07-04 16:32:53 +0500

    First commit

---
+++ hello.c
@@ -0,0 +1,20 @@
+#include <curses.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <emscripten.h>
+
+int main()
+{
+
+  trace(TRACE_MAXIMUM);
+
+  initscr();      /* Start curses mode      */
+  printw("Hello World !!!\n");  /* Print Hello World      */
+  //printw("Hello World !!!\n\rFOOOBAR\n");  /* Print Hello World      */
+  //printw("BAZ");  /* Print Hello World      */
+  refresh();      /* Print it on to the real screen */
+  //getch();      /* Wait for user input */
+  endwin();     /* End curses mode      */
+
+  return 0;
+}
