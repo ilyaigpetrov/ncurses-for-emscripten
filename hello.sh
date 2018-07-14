3b9e668eb9635a4cc4fd416d77e54485b0bfc960
ilyaigpetrov <ilyaigpetrov@gmail.com>
2018-07-14 20:43:58 +0500

    Replant configs from https://github.com/ilyaigpetrov/ncurses-for-emscripten/issues/1#issuecomment-405029179

--- hello.sh
+++ hello.sh
@@ -1,4 +1,11 @@
-# source ../../emsdk_env.sh
-emcc ./hello.c -I ncurses-6.1-llvm/test -L ./ncurses-6.1-llvm/lib -I ./ncurses-6.1-llvm/include -lncurses_g --preload-file lib/terminfo@/home/web_user/.terminfo -o hello.html -s FORCE_FILESYSTEM=1 -Ugetenv -D"getenv(name)=(char*) EM_ASM_INT ( {var envar = JSON.stringify(name);var ret=allocate(intArrayFromString(envar), 'i8', ALLOC_NORMAL);return ret ;},NULL)" --shell-file ./min-shell.html -o out.html -g4 -s ASSERTIONS=2 -s WASM=1 -Werror -s ALLOW_MEMORY_GROWTH=1 --emrun
-emrun --no_browser --port 8080 .
-
+emcc ./hello.c \
+  -L ./ncurses-6.1-llvm/lib \
+  -I ./ncurses-6.1-llvm/include \
+  -lncurses_g \
+  --preload-file lib/terminfo@/home/web_user/.terminfo \
+  -o compiled.js \
+  -Ugetenv \
+  -D"getenv(name)=(char*) EM_ASM_INT ( {var envar = JSON.stringify(name);var ret=allocate(intArrayFromString(envar), 'i8', ALLOC_NORMAL);return ret ;},NULL)" \
+  -g4 \
+  -s WASM=1 \
+  -s ALLOW_MEMORY_GROWTH=1
