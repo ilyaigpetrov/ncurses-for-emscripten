ed697b17ae916bb585d53d5e236e337201717928
ilyaigpetrov <ilyaigpetrov@gmail.com>
2018-07-14 21:13:08 +0500

    Add scripts that try to compile worm.c and getch examples

---
+++ hello2.sh
@@ -0,0 +1,12 @@
+emcc ./hello.c \
+  -L ./ncurses-6.1-llvm/lib \
+  -I ./ncurses-6.1-llvm/include \
+  -lncurses_g \
+  --preload-file lib/terminfo@/home/web_user/.terminfo \
+  -o hello.html \
+  -Ugetenv \
+  -D"getenv(name)=(char*) EM_ASM_INT ( {var envar = JSON.stringify(name);var ret=allocate(intArrayFromString(envar), 'i8', ALLOC_NORMAL);return ret ;},NULL)" \
+  -g4 \
+  -s WASM=1 \
+  -s ALLOW_MEMORY_GROWTH=1 \
+  --shell-file ./min-shell.html
